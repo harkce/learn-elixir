0c6926f0d398630babec786c314ee84464bc07dc
Habib Fikri <habibgarut@gmail.com>
2019-08-13 16:55:54 +0700

    Add pick color

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,16 +5,15 @@ defmodule Identicon do
     |> pick_color
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
-    [r, g, b]
-  end
-
   def hash_input(input) do
     %Identicon.Image{
       hex:
         :crypto.hash(:md5, input)
         |> :binary.bin_to_list()
     }
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 end
